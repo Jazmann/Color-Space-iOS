fd3377f2eb63afb48acd98e82647c802b3256e79
Jasper <jaspershemilt@virginmedia.co.uk>
2014-09-29 17:28:33 +0100

    tidyup

--- matLab/ColorSpaceStats/rgbToMono.m
+++
@@ -1,8 +0,0 @@
-function channelImage = rgbToMono(img,chan)
-myType = class(img);
-[rows, cols, chans] = size(img);
-%# Shift each color plane (stored in each column of the N-by-3 matrix):
-channelImage = cast(zeros(rows, cols, 3),myType);
-channelImage(:,:,mod(chan - 1,3)+1) = img(:,:,chan);
-
-end % function
--- matLab/ColorSpaceStats/transformationMatrix.m
+++
@@ -1,8 +0,0 @@
-function transformMatrix = transformationMatrix(Kb, Kr, theta, yScale, bScale, rScale)
-% Matrix multiplication with RGB 0:1 gives YCbCr 0:1 -0.5:0.5 -0.5:0.5
-A = [Kr.*yScale,                             (1+(-1).*Kb+(-1).*Kr).*yScale,                             Kb.*yScale;...
-     (-1/2).*bScale.*(1+(-1).*Kb).^(-1).*Kr, (-1/2).*bScale.*(1+(-1).*Kb).^(-1).*(1+(-1).*Kb+(-1).*Kr), (1/2).*bScale;...
-     (1/2).*rScale,                          (-1/2).*(1+(-1).*Kr).^(-1).*(1+(-1).*Kb+(-1).*Kr).*rScale, (-1/2).*Kb.*(1+(-1).*Kr).^(-1).*rScale ]; 
-transformMatrix = A' * [ 1, 0, 0; 0, cos(theta), sin(theta); 0, -sin(theta), cos(theta)]';
-end
-
